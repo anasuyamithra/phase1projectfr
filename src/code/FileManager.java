dc66743dde55b0867ba77095614edeafd76b3fc1
Anasuya Mithra <anasuya.mithra@gmail.com>
2023-06-13 14:27:33 +0530

    BST implementation for optimisation

--- src/code/FileManager.java
+++ src/code/FileManager.java
@@ -3,150 +3,139 @@ package code;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Arrays;
-import java.io.FileWriter;
 import java.util.Scanner;
+import java.io.FileWriter;
 
 public class FileManager {
+    private TreeNode root;
 
-    private void quicksort(String[] arr, int low, int high) {
-        if (low < high) {
-            int pivotIndex = partition(arr, low, high);
-            quicksort(arr, low, pivotIndex - 1);
-            quicksort(arr, pivotIndex + 1, high);
+    private static class TreeNode {
+        String fileName;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(String fileName) {
+            this.fileName = fileName;
         }
     }
 
-    private int partition(String[] arr, int low, int high) {
-        String pivot = arr[high];
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (arr[j].compareTo(pivot) < 0) {
-                i++;
-                swap(arr, i, j);
-            }
-        }
-
-        swap(arr, i + 1, high);
-        return i + 1;
-    }
-
-    private void swap(String[] arr, int i, int j) {
-        String temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-	
     public void displayFileNames() {
-        File rootDirectory = new File(".");
-        String[] fileNames = rootDirectory.list();
-        quicksort(fileNames, 0, fileNames.length - 1);
+    	System.out.println("Inside file manager display function");
+        inOrderTraversal(root);
+    }
 
-        System.out.println("\nCurrent file names in ascending order:");
-        for (String fileName : fileNames) {
-            System.out.println(fileName);
+    private void inOrderTraversal(TreeNode node) {
+        if (node != null) {
+            inOrderTraversal(node.left);
+            System.out.println(node.fileName);
+            inOrderTraversal(node.right);
+        } else {
+        	System.out.println("Directory is empty. Please add some files.");
         }
     }
-    
-//    public String[] getFileNames() {
-//        File folder = new File(directory);
-//        File[] files = folder.listFiles();
-//
-//        if (files == null) {
-//            return new String[0];
-//        }
-//
-//        String[] fileNames = new String[files.length];
-//        for (int i = 0; i < files.length; i++) {
-//            fileNames[i] = files[i].getName();
-//        }
-//
-//        quicksort(fileNames, 0, fileNames.length - 1);
-//
-//        return fileNames;
-//    }
 
-    public void addFile(String fileName) throws Exception {
-        // Add logic to add the file
-        //throw new UnsupportedOperationException("Add file operation not implemented yet.");
-    	 File directory = new File(".");
-         File[] files = directory.listFiles();
+    public void addFile(String fileName) throws IOException {
+        File newFile = new File(fileName);
 
-         if (files != null) {
-             for (File file : files) {
-                 if (file.getName().equalsIgnoreCase(fileName)) {
-                     throw new IOException("File already exists: " + fileName);
-                 }
-             }
-         }
+        if (newFile.createNewFile()) {
+            System.out.println("File created successfully: " + fileName);
+            insertNode(fileName);
+            System.out.println("Enter content for the file (press Enter on an empty line to finish):");
 
-         File newFile = new File(fileName);
+            try (FileWriter writer = new FileWriter(newFile)) {
+                Scanner scanner = new Scanner(System.in);
+                String line;
 
-         if (newFile.createNewFile()) {
-             System.out.println("File created successfully: " + fileName);
-             System.out.println("Enter content for the file (press Enter on an empty line to finish):");
+                while (!(line = scanner.nextLine()).isEmpty()) {
+                    writer.write(line);
+                    writer.write(System.lineSeparator());
+                }
 
-             try (FileWriter writer = new FileWriter(newFile)) {
-                 Scanner scanner = new Scanner(System.in);
-                 String line;
-
-                 while (!(line = scanner.nextLine()).isEmpty()) {
-                     writer.write(line);
-                     writer.write(System.lineSeparator());
-                 }
-
-                 System.out.println("Content saved to the file.");
-             }
-         } else {
-             throw new IOException("Failed to create the file: " + fileName);
-         }
-    }
-
-    public void deleteFile(String fileName) throws Exception {
-        File file = new File(fileName);
-
-        if (file.exists()) {
-            boolean deleted = file.delete();
-            if (!deleted) {
-                throw new Exception("Failed to delete the file.");
+                System.out.println("Content saved to the file.");
             }
         } else {
+            throw new IOException("Failed to create the file: " + fileName);
+        }
+    }
+
+
+    private void insertNode(String fileName) {
+        if (root == null) {
+            root = new TreeNode(fileName);
+        } else {
+            insertNode(root, fileName);
+        }
+    }
+
+    private void insertNode(TreeNode node, String fileName) {
+        if (fileName.compareTo(node.fileName) < 0) {
+            if (node.left == null) {
+                node.left = new TreeNode(fileName);
+            } else {
+                insertNode(node.left, fileName);
+            }
+        } else {
+            if (node.right == null) {
+                node.right = new TreeNode(fileName);
+            } else {
+                insertNode(node.right, fileName);
+            }
+        }
+    }
+
+    public void deleteFile(String fileName) throws FileNotFoundException {
+        root = deleteNode(root, fileName);
+    }
+
+    private TreeNode deleteNode(TreeNode node, String fileName) throws FileNotFoundException {
+        if (node == null) {
             throw new FileNotFoundException("File not found.");
         }
+
+        if (fileName.compareTo(node.fileName) < 0) {
+            node.left = deleteNode(node.left, fileName);
+        } else if (fileName.compareTo(node.fileName) > 0) {
+            node.right = deleteNode(node.right, fileName);
+        } else {
+            if (node.left == null) {
+                return node.right;
+            } else if (node.right == null) {
+                return node.left;
+            }
+
+            node.fileName = findMinimum(node.right).fileName;
+            node.right = deleteNode(node.right, node.fileName);
+        }
+
+        return node;
+    }
+
+    private TreeNode findMinimum(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
     }
 
     public void searchFile(String fileName) {
-    	File directory = new File(".");
-        String[] fileNames = directory.list();
-        
-        if (fileNames.length == 0) {
-            System.out.println("The directory is empty.");
-            return;
-        }
-        quicksort(fileNames, 0, fileNames.length - 1);
-        
-        if(binarySearch(fileNames, fileName, 0, fileNames.length - 1)) {
-        	System.out.println("File exists.");
+        if (searchFile(root, fileName)) {
+            System.out.println("File exists.");
         } else {
-        	System.out.println("File not found.");
+            System.out.println("File not found.");
         }
-        
     }
 
-    private boolean binarySearch(String[] arr, String key, int low, int high) {
-        if (low <= high) {
-            int mid = low + (high - low) / 2;
-            int result = key.compareTo(arr[mid]);
-
-            if (result == 0) {
-                return true;
-            } else if (result < 0) {
-                return binarySearch(arr, key, low, mid - 1);
-            } else {
-                return binarySearch(arr, key, mid + 1, high);
-            }
+    private boolean searchFile(TreeNode node, String fileName) {
+        if (node == null) {
+            return false;
         }
-        return false;
+
+        if (fileName.compareTo(node.fileName) == 0) {
+            return true;
+        } else if (fileName.compareTo(node.fileName) < 0) {
+            return searchFile(node.left, fileName);
+        } else {
+            return searchFile(node.right, fileName);
+        }
     }
 }
